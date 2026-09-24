62f41a10fc3841c8c1fc779556cc127add0972c6
Neev Uttamchandani <neevchandani05@gmail.com>
2026-09-24 17:49:00 +0530

    Time: 3 ms (89.34%), Space: 23.8 MB (60.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,54 +1,17 @@
 class Solution {
 public:
-    int partition(vector<vector<int>>& intervals,int low,int high)
-    {
-        int pivot=intervals[low][0];
-        int i=low;
-        int j=high;
-        while(i<j)
-        {
-            while(intervals[i][0]<=pivot && i<=high-1)
-            i++;
-            while(intervals[j][0]>=pivot &&j>=low+1)
-            j--;
-            if(i<j)
-            {
-            swap(intervals[i][0],intervals[j][0]);
-            swap(intervals[i][1],intervals[j][1]);
-            }
-        }
-        swap(intervals[low][0],intervals[j][0]);
-        swap(intervals[low][1],intervals[j][1]);
-        return j;
-
-    }
-    void qs(vector<vector<int>>&intervals,int low ,int high)
-    {
-        if(low<high)
-        {
-            int p=partition(intervals,low,high);
-            qs(intervals,low,p-1);
-            qs(intervals,p+1,high);
-        }
-    }
-    void quicksort(vector<vector<int>> &intervals)
-    {
-        qs(intervals,0,intervals.size()-1);
-    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        quicksort(intervals);
-        for(int i=1;i<intervals.size();i++)
+       
+       if (intervals.empty()) return {};
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> merged;
+        for( auto& interval:intervals)
         {
-            if(intervals[i][0]<=intervals[i-1][1])
-            {
-                intervals[i][0]=intervals[i-1][0];
-                intervals[i][1]=max(intervals[i][1],intervals[i-1][1]);
-                intervals.erase(intervals.begin()+i-1);
-                i--;
-            }
-        }
-        return intervals;
-        
-    }
+            if(merged.empty()|| merged.back()[1]<interval[0])
+            merged.push_back(interval);
+            else
+            merged.back()[1]=max(merged.back()[1],interval[1]);
+        }   
+        return merged;
+       }
 };
